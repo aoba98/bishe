91708a068885a58e829b43e16763204ffdbf2ebb
Jinkin <nadph513@outlook.com>
2020-07-26 13:45:05 +0800

    修改LSTM部分

---
+++ sripts/import_file.py
@@ -0,0 +1,14 @@
+# model & data construct
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+# plot 
+import matplotlib as mpl
+import matplotlib as plt
+
+# jupyter notebook 
+# %matplotlib inline
+
+# other
+import time
