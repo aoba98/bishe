6ef70efac6ff7cdd5f7f212436ee2966039ceb02
Jinkin <jinkin@JinkindeiMac.local>
2020-07-28 09:50:21 +0800

    修改网络

---
+++ scripts/import_file.py
@@ -0,0 +1,16 @@
+# model & data construct
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# plot 
+import matplotlib as mpl
+import matplotlib as plt
+
+# jupyter notebook 
+# %matplotlib inline
+
+# other
+import time
